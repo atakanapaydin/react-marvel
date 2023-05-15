032b9d2a255457c9fe18534611bbf430b58a57bf
atakanapaydin <66443801+atakanapaydin@users.noreply.github.com>
2023-05-15 21:21:18 +0300

    create project.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+function App() {
+  return (
+    <div className="App">
+      <div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
